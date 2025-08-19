77baf6f41562c34a7289cee1fd529ba8047c5ebe
roidm <roidm@protonmail.com>
2025-08-19 20:06:30 +0200

    add picture

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,17 +1,16 @@
--- Opciones básicas de Neovim
-
--- Líneas de números
+-- Basic Neovim options
 vim.opt.nu = true -- Número de línea normal
 vim.opt.relativenumber = false -- Número de línea relativo
 
--- Sangría (indentation)
+-- Indentation
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true -- Usa espacios en lugar de tabulaciones
 
 vim.opt.switchbuf = "newtab"
 vim.opt.showtabline = 0
--- Búsqueda
+
+-- Search
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 
@@ -20,8 +19,17 @@ vim.opt.termguicolors = true -- Permite usar colores verdaderos
 vim.opt.cursorline = true -- Resalta la línea actual
 vim.opt.scrolloff = 8 -- Mantiene 8 líneas de contexto al desplazarse
 
--- Teclas
+-- Keys
 vim.g.mapleader = " " -- Setea el líder a la barra espaciadora
 
--- Sincroniza el portapapeles de Neovim con el del sistema
+-- clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- Shada history
+vim.opt.shada = {
+  "!",    -- Saves the command history
+  "'100",  -- Search history (100 elements)
+  "@100", -- Registers (100 elements)
+  "<50",   -- Line history (50 elements)
+  "f10",  -- Recent files (10 elements)
+}
